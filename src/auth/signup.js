da0fc830e1101b821959bba05d20e94f1642f041
advanceJs <emmanuelolushola108@gmail.com>
2026-05-14 16:26:31 +0100

    ui correction(auth page)

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -1,7 +1,14 @@
 const signUpBtns = document.querySelectorAll(".open_acc_btn");
-const signUpLink = document.querySelector(".sign_up_link");
+const signUpLink = document.querySelector(".sign_suggestion");
+const loginLink = document.querySelector(".login_suggestion");
 const signUpPage = document.querySelector(".signup_flex");
 const loginPage = document.querySelector(".login_flex");
+function clearClass() {
+  loginPage.classList.remove("login_hide");
+  signUpPage.classList.remove("signup_show");
+  signUpPage.classList.remove("signup_hide");
+  loginPage.classList.remove("login_show");
+}
 signUpBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -10,6 +17,13 @@ signUpBtns.forEach((btn) => {
 });
 signUpLink.addEventListener("click", (e) => {
   e.preventDefault();
-  loginPage.classList.add("remove_login");
-  signUpPage.classList.add("sign_up");
+  clearClass();
+  loginPage.classList.add("login_hide");
+  signUpPage.classList.add("signup_show");
+});
+loginLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearClass();
+  signUpPage.classList.add("signup_hide");
+  loginPage.classList.add("login_show");
 });
